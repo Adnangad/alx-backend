bf11caaa830ed3aa77554475b01521a8a57fc6a2
Adnangad <gardobuyaadnan@gmail.com>
2024-07-21 10:35:21 +0000

    1-added index-range method

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -22,23 +22,24 @@ class Server:
 
         return self.__dataset
 
+    def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
+        """
+        Args:
+        page: page number
+        page_size: size of contents in page or page limit
+        """
+        start: int = (page - 1) * page_size
+        end: int = start + page_size
+        return(start, end)
+    
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
         Returns pages
         """
-        def index_range(page: int, page_size: int) -> Tuple[int, int]:
-            """
-            Args:
-            page: the page number
-            page_size: the total number of items in the page
-            """
-            start: int = (page - 1) * page_size
-            end: int = start + page_size
-            return(start, end)
         if not isinstance(page, int) or not isinstance(page_size, int):
             raise AssertionError("Both page and page_size must be an integer")
         if page <= 0 or page_size<= 0:
             raise AssertionError("Both page and page_siz must be > 0")
         data = self.dataset()
-        start, end = index_range(page, page_size)
+        start, end = self.index_range(page, page_size)
         return data[start:end]
