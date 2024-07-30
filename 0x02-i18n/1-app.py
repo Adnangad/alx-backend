ce6dcb535666bb037d3f97cfbf5469c913a1a743
Adnangad <gardobuyaadnan@gmail.com>
2024-07-30 12:45:45 +0300

    1-babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" This module initializes a web app"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """ Configs the lang"""
+    LANGUAGES = ["en", "fr"]
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+
+
+@app.route("/")
+def home_pg():
+    """ Redirects the user to the homepage"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
