594737fc1a86360a0d5767efe330a1edfa47abfa
Adnangad <gardobuyaadnan@gmail.com>
2024-07-30 19:31:47 +0300

    3-translator

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" This module initializes a web app"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+class Config:
+    """ Configures the language and locale"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    """Gets the best match for the user's locale."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route("/")
+def home_pg():
+    """Renders the home page."""
+    return render_template('3-index.html')
+
+if __name__ == '__main__':
+    app.run()
