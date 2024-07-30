352955cbc7b93588f038bca885b520778b336977
Adnangad <gardobuyaadnan@gmail.com>
2024-07-30 19:36:14 +0300

    3-translator

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python3
 """ This module initializes a web app"""
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
-class Config:
+
+class Config(object):
     """ Configures the language and locale"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale():
     """Gets the best match for the user's locale."""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-@app.route("/")
+
+@app.route("/", strict_slashes=False)
 def home_pg():
     """Renders the home page."""
     return render_template('3-index.html')
+
 
 if __name__ == '__main__':
     app.run()
