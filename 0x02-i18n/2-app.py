52682f24fe3a2b8bf6f93811b4cb96d5eb90a009
Adnangad <gardobuyaadnan@gmail.com>
2024-07-30 13:06:21 +0300

    2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" This module initializes a web app"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """ Configs the lang"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT__LOCALE = 'en'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ Gets the default locale"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def home_pg():
+    """ Redirects the user to the homepage"""
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
